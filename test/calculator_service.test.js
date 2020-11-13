7fc591b5c098083cc9303e34059487f55d94e9c2
isakulaksiz <isakulaksiz@outlook.com>
2020-11-13 11:38:00 +0300

    Add further test cases

--- test/calculator_service.test.js
+++ test/calculator_service.test.js
@@ -14,3 +14,18 @@ describe("test to see if functions are defined", () => {
         expect(getTime).not.toBeUndefined
     })
 });
+
+describe("test to check if the functions returns a correct output when given an input", () => {
+    
+    test("test add function", () => {
+        expect(add(0,10)).toBe(10);
+    })
+
+    test("test subtract funtion", () => {
+        expect(subtract(10,2)).toBe(8);
+    })
+
+    test("test getTime function", () => {
+        expect(getTime()).not.toBeUndefined;
+    })
+});
